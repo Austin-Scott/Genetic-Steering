5d381fc123eda220ee7d74119abb8b7336c13bdc
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2019-04-26 17:33:40 -0500

    Tweeking some values

---
+++ Assets/Scripts/PeekWindowController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PeekWindowController : MonoBehaviour {
+
+    public Text factionText;
+    public Text stateText;
+    public Text fuelText;
+
+    private bool isVisible=false;
+    private Boid boidSelected = null;
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
